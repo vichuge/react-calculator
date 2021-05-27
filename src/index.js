4800119d304fc255a6f004d2aac8b5bd48383ce8
V <victor.hugo.pacheco.flores@gmail.com>
2021-05-27 14:09:44 -0500

    Add and fix Linters issues

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-class Greeting extends React.Component {
-  render() {
-    return (
-      <h1>Hello, {this.props.name}</h1>
-    );
-  }
+function Greeting({ name }) {
+  return (
+    <h1>
+      {' '}
+      Hello,
+      {name}
+    </h1>
+  );
 }
 
+Greeting.defaultProps = {
+  name: 'Defatult',
+};
+
 Greeting.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
 };
 
 ReactDOM.render(
-    <Greeting name="Victor" />,
-    document.getElementById('root')
+  <Greeting name="Victor" />,
+  document.getElementById('root'),
 );
