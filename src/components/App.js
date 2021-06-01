9a68aebbe12b89654f86380a2a3c44897eedd97a
V <victor.hugo.pacheco.flores@gmail.com>
2021-06-01 17:35:05 -0500

    Convert app.js in class

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,25 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <h1>Calculator</h1>
-      <Display result={calculate.total} />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Calculator</h1>
+        <Display result={calculate.total} />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;
