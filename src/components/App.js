54ed3fd7856a2de86bf643e53aa032a2535452e1
V <victor.hugo.pacheco.flores@gmail.com>
2021-06-10 14:05:14 -0500

    Change class to function on App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = ({ total: initialTotal, next: initialNext, operation: initialOperation }) => {
+  const [total, setTotal] = useState(initialTotal);
+  const [next, setNext] = useState(initialNext);
+  const [operation, setOperation] = useState(initialOperation);
 
-  handleClick(buttonName) {
-    const result = calculate(this.state, buttonName);
-    this.setState({ ...result });
-  }
+  const handleClick = (buttonName) => {
+    const result = calculate({ total, next, operation }, buttonName);
+    setTotal(result.total);
+    setNext([result.next]);
+    setOperation([result.operation]);
+  };
 
-  render() {
-    const { total, operation, next } = this.state;
-    let display = '';
-    if (!total) display = '0';
-    if (total) display = total;
-    if (total && operation) display = total;
-    if (total && operation && next) display = next;
-    return (
-      <>
-        <Display result={display} />
-        <ButtonPanel clickApp={this.handleClick} />
-      </>
-    );
-  }
-}
+  let display = '';
+  if (!total) display = '0';
+  if (total) display = total;
+  if (total && operation) display = total;
+  if (total && operation && next) display = next;
+
+  return (
+    <>
+      <Display result={display} />
+      <ButtonPanel clickApp={handleClick} />
+    </>
+  );
+};
+
+App.propTypes = {
+  total: PropTypes.string,
+  next: PropTypes.string,
+  operation: PropTypes.string,
+};
+
+App.defaultProps = {
+  total: null,
+  next: null,
+  operation: null,
+};
 
 export default App;
