4830261a3da21b72ec2bdd55288c703885ceef13
Victor Pacheco <victor.hugo.pacheco.flores@gmail.com>
2021-06-17 11:48:15 -0500

    Add operate.js tests

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -17,9 +17,25 @@ it('return result 2', () => {
 });
 
 it('return result 3', () => {
+  const total = '5';
+  const next = '2';
+  const operation = 'X';
+  const t = operate(total, next, operation);
+  expect(t).toBe('10');
+});
+
+it('return result 4', () => {
   const total = '12';
   const next = '4';
   const operation = '/';
   const t = operate(total, next, operation);
   expect(t).toBe('3');
+});
+
+it('return result 5', () => {
+  const total = '10';
+  const next = '3';
+  const operation = '%';
+  const t = operate(total, next, operation);
+  expect(t).toBe('1');
 });
